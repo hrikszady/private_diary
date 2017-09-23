e54769cc402f5e67f2a3b0203aad5a1d2769b9be
hriks <hriks@outlook.com>
2017-09-23 22:29:03 +0530

    update models and save password as bycrypt

--- base/models.py
+++ base/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.utils.crypto import get_random_string
 from django.db import models
 import uuid
 
@@ -12,11 +12,15 @@ class User(models.Model):
     and providing offers and premium facility.
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    reference_no = models.CharField(default=None, max_length=10)
+    reference_no = models.CharField(default="None", max_length=10)
+    username = models.CharField(
+        default=None, max_length=10)
+    password = models.CharField(max_length=512)
     name = models.CharField(max_length=20)
-    phone_no = models.IntegerField(
-        default=0000000000, blank=False)
-    alternate_phone_no = models.IntegerField(default=0000000000)
+    phone_no = models.CharField(
+        default=0000000000, blank=False, max_length=10)
+    alternate_phone_no = models.CharField(
+        default=0000000000, blank=True, max_length=10)
     std_code = models.IntegerField(default=000000)
     COUNTRY_CHOICES = [
         ("india", "IND"), ("pakistan", "PAK"), ("bangaldesh", "BAN"),
@@ -35,29 +39,65 @@ class User(models.Model):
     hobbies = models.TextField(null=True, blank=True)
     is_approved_terms_and_conditions = models.BooleanField(default=False)
 
+    def save(self, *args, **kwargs):
+        try:
+            current = User.objects.get(id=self.id)
+            if not current.check_password(str(self.password)):
+                self.password = self.get_hashed_password(str(self.password))
+        except User.DoesNotExist:
+            if self.reference_no == "None":
+                temp_ref = get_random_string(
+                    10, allowed_chars='ABCDEFGHITUVWXYZ0123456789')
+                users = User.objects.filter(reference_no=temp_ref)
+                while users.exists():
+                    temp_ref = get_random_string(
+                        10, allowed_chars='ABCDEFGHITUVWXYZ0123456789') # noqa
+                self.reference_no = temp_ref
+            self.password = self.get_hashed_password(str(self.password))
+        super(User, self).save(*args, **kwargs)
 
-class Academics(models.Model):
+    def get_hashed_password(self, password):
+        """Hash a password for the first time
+        Using bcrypt, the salt is saved into the hash itself
+        """
+        import bcrypt
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(password, salt)
+
+    def check_password(self, password):
+        """Check hased password. Using bcrypt,
+        the salt is saved into the hash itself
+        """
+        import bcrypt
+        return bcrypt.hashpw(
+            password, str(self.password)) == str(self.password)
+
+
+class Academic(models.Model):
     """Stores for Academics details for registered users"""
     user = models.ForeignKey('user')
     year_of_passing = models.DateTimeField(null=True, blank=True)
     percentage = models.FloatField(default=0)
-    institute = models.CharField(default=256, max_length=256)
-    location = models.CharField(default=128, max_length=64)
+    institute = models.CharField(default=None, max_length=256)
+    location = models.CharField(default=None, max_length=64)
 
 
-class ExtraUserInfo(models.Model):
+class ParentalInfo(models.Model):
     """
     Class ExtraPersonalInfo is created for those
     users who wish to create resume, they have to fill
     these extra options
     """
     user = models.ForeignKey('user')
-    fathers_name = models.CharField(blank=False, max_length=64)
-    mothers_name = models.CharField(blank=True, max_length=64)
-    fathers_phone = models.CharField(blank=False, max_length=64)
+    fathers_name = models.CharField(blank=False, max_length=32)
+    mothers_name = models.CharField(blank=True, max_length=32)
+    fathers_phone = models.CharField(
+        default=0000000000, blank=False, max_length=10)
+    guardian_phone = models.CharField(
+        default=0000000000, blank=False, max_length=10)
 
 
-class Documents(models.Model):
+class Document(models.Model):
     """
     Stores Documents for users, if case they need
     user can download their documents
@@ -75,7 +115,7 @@ class Documents(models.Model):
     digital_validation = models.BooleanField(default=False)
 
 
-class Expenses(models.Model):
+class Expense(models.Model):
     """
     User can keep track for their expenses
     """
@@ -83,10 +123,12 @@ class Expenses(models.Model):
     amount = models.FloatField(default=0)
     expense_date = models.DateTimeField(auto_now_add=False)
     CATEGORY_CHOICES = [
-        ("select", "CHOOSE"), ("food", "FOOD"), ("bill_payments", "BILL & PAYMENTS"),
-        ("shopping_purchases", "SHOPPING & PURCHASES"), ("lended", "LENDING"),
-        ("fare", "FARE"), ("donation", "DONATION"), ("fees", "FEES"), ("fine", "FINE"),
-        ("movie", "MOVIE")
+        ("select", "CHOOSE"), ("food", "FOOD"),
+        ("bill_payments", "BILL & PAYMENTS"),
+        ("shopping_purchases", "SHOPPING & PURCHASES"),
+        ("lended", "LENDING"), ("fare", "FARE"),
+        ("donation", "DONATION"), ("fees", "FEES"),
+        ("fine", "FINE"), ("movie", "MOVIE")
     ]
     category = models.CharField(
         default="select", choices=CATEGORY_CHOICES, max_length=12)
@@ -112,7 +154,8 @@ class Donation(models.Model):
     transaction_id = models.CharField(blank=False, default=None, max_length=64)
     amount = models.FloatField(blank=False, default=0)
     comments = models.TextField()
-    transaction_mode = models.CharField(blank=False, default=None, max_length=64)
+    transaction_mode = models.CharField(
+        blank=False, default=None, max_length=64)
     created = models.DateTimeField(auto_now_add=True)
 
 
@@ -123,6 +166,3 @@ class LastAction(object):
     user = models.ForeignKey('user')
     action = models.CharField(blank=False, max_length=128)
     created = models.DateTimeField(auto_now_add=True)
-        
-
-        
