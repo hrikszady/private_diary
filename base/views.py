5a2ce76878b4461bcfe715437ca7e0fbc44e3135
kartavyasingh <krank41@gmail.com>
2017-09-26 13:07:12 +0530

    rendering template (#16)

--- base/views.py
+++ base/views.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect
-from methods import *
-
+from django.views.generic import TemplateView
+#from methods import *
 
 def login(request):
 	return render(request, 'templates/')
+
+class HomePageView(TemplateView):
+	def get(self,request,*args,**kwargs):
+		return render(request,'index.html',None)
