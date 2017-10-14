473efef91b370cc21d9285155f362cec090e51e2
Amit Kumar Gupta <hriks@users.noreply.github.com>
2017-10-15 01:11:36 +0530

    Hriks (#21)
    * Removed remarks from JS
    
    * Changed index page content from boiler plate to useable

--- base/static/lib/bootstrap/js/bootstrap.js
+++ base/static/lib/bootstrap/js/bootstrap.js
@@ -1,9 +1,3 @@
-/*!
- * Bootstrap v3.3.7 (http://getbootstrap.com)
- * Copyright 2011-2016 Twitter, Inc.
- * Licensed under the MIT license
- */
-
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
@@ -15,22 +9,8 @@ if (typeof jQuery === 'undefined') {
     throw new Error('Bootstrap\'s JavaScript requires jQuery version 1.9.1 or higher, but lower than version 4')
   }
 }(jQuery);
-
-/* ========================================================================
- * Bootstrap: transition.js v3.3.7
- * http://getbootstrap.com/javascript/#transitions
- * ========================================================================
- * Copyright 2011-2016 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
-
-
 +function ($) {
   'use strict';
-
-  // CSS TRANSITION SUPPORT (Shoutout: http://www.modernizr.com/)
-  // ============================================================
-
   function transitionEnd() {
     var el = document.createElement('bootstrap')
 
@@ -40,17 +20,13 @@ if (typeof jQuery === 'undefined') {
       OTransition      : 'oTransitionEnd otransitionend',
       transition       : 'transitionend'
     }
-
     for (var name in transEndEventNames) {
       if (el.style[name] !== undefined) {
         return { end: transEndEventNames[name] }
       }
     }
-
     return false // explicit for ie8 (  ._.)
   }
-
-  // http://blog.alexmaccaw.com/css-transitions
   $.fn.emulateTransitionEnd = function (duration) {
     var called = false
     var $el = this
@@ -59,7 +35,6 @@ if (typeof jQuery === 'undefined') {
     setTimeout(callback, duration)
     return this
   }
-
   $(function () {
     $.support.transition = transitionEnd()
 
@@ -75,70 +50,38 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
-
-/* ========================================================================
- * Bootstrap: alert.js v3.3.7
- * http://getbootstrap.com/javascript/#alerts
- * ========================================================================
- * Copyright 2011-2016 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
-
-
 +function ($) {
   'use strict';
-
-  // ALERT CLASS DEFINITION
-  // ======================
-
   var dismiss = '[data-dismiss="alert"]'
   var Alert   = function (el) {
     $(el).on('click', dismiss, this.close)
   }
-
   Alert.VERSION = '3.3.7'
-
   Alert.TRANSITION_DURATION = 150
-
   Alert.prototype.close = function (e) {
     var $this    = $(this)
     var selector = $this.attr('data-target')
-
     if (!selector) {
       selector = $this.attr('href')
       selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
     }
-
     var $parent = $(selector === '#' ? [] : selector)
-
     if (e) e.preventDefault()
-
     if (!$parent.length) {
       $parent = $this.closest('.alert')
     }
-
     $parent.trigger(e = $.Event('close.bs.alert'))
-
     if (e.isDefaultPrevented()) return
-
     $parent.removeClass('in')
-
     function removeElement() {
-      // detach from parent, fire event then clean up data
       $parent.detach().trigger('closed.bs.alert').remove()
     }
-
     $.support.transition && $parent.hasClass('fade') ?
       $parent
         .one('bsTransitionEnd', removeElement)
         .emulateTransitionEnd(Alert.TRANSITION_DURATION) :
       removeElement()
   }
-
-
-  // ALERT PLUGIN DEFINITION
-  // =======================
-
   function Plugin(option) {
     return this.each(function () {
       var $this = $(this)
@@ -148,67 +91,33 @@ if (typeof jQuery === 'undefined') {
       if (typeof option == 'string') data[option].call($this)
     })
   }
-
   var old = $.fn.alert
-
   $.fn.alert             = Plugin
   $.fn.alert.Constructor = Alert
-
-
-  // ALERT NO CONFLICT
-  // =================
-
   $.fn.alert.noConflict = function () {
     $.fn.alert = old
     return this
   }
-
-
-  // ALERT DATA-API
-  // ==============
-
   $(document).on('click.bs.alert.data-api', dismiss, Alert.prototype.close)
-
 }(jQuery);
-
-/* ========================================================================
- * Bootstrap: button.js v3.3.7
- * http://getbootstrap.com/javascript/#buttons
- * ========================================================================
- * Copyright 2011-2016 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
-
-
 +function ($) {
   'use strict';
-
-  // BUTTON PUBLIC CLASS DEFINITION
-  // ==============================
-
   var Button = function (element, options) {
     this.$element  = $(element)
     this.options   = $.extend({}, Button.DEFAULTS, options)
     this.isLoading = false
   }
-
   Button.VERSION  = '3.3.7'
-
   Button.DEFAULTS = {
     loadingText: 'loading...'
   }
-
   Button.prototype.setState = function (state) {
     var d    = 'disabled'
     var $el  = this.$element
     var val  = $el.is('input') ? 'val' : 'html'
     var data = $el.data()
-
     state += 'Text'
-
     if (data.resetText == null) $el.data('resetText', $el[val]())
-
-    // push to event loop to allow forms to submit
     setTimeout($.proxy(function () {
       $el[val](data[state] == null ? this.options[state] : data[state])
 
@@ -221,11 +130,9 @@ if (typeof jQuery === 'undefined') {
       }
     }, this), 0)
   }
-
   Button.prototype.toggle = function () {
     var changed = true
     var $parent = this.$element.closest('[data-toggle="buttons"]')
-
     if ($parent.length) {
       var $input = this.$element.find('input')
       if ($input.prop('type') == 'radio') {
@@ -243,11 +150,6 @@ if (typeof jQuery === 'undefined') {
       this.$element.toggleClass('active')
     }
   }
-
-
-  // BUTTON PLUGIN DEFINITION
-  // ========================
-
   function Plugin(option) {
     return this.each(function () {
       var $this   = $(this)
@@ -265,20 +167,10 @@ if (typeof jQuery === 'undefined') {
 
   $.fn.button             = Plugin
   $.fn.button.Constructor = Button
-
-
-  // BUTTON NO CONFLICT
-  // ==================
-
   $.fn.button.noConflict = function () {
     $.fn.button = old
     return this
   }
-
-
-  // BUTTON DATA-API
-  // ===============
-
   $(document)
     .on('click.bs.button.data-api', '[data-toggle^="button"]', function (e) {
       var $btn = $(e.target).closest('.btn')
@@ -296,22 +188,8 @@ if (typeof jQuery === 'undefined') {
     })
 
 }(jQuery);
-
-/* ========================================================================
- * Bootstrap: carousel.js v3.3.7
- * http://getbootstrap.com/javascript/#carousel
- * ========================================================================
- * Copyright 2011-2016 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
-
-
 +function ($) {
   'use strict';
-
-  // CAROUSEL CLASS DEFINITION
-  // =========================
-
   var Carousel = function (element, options) {
     this.$element    = $(element)
     this.$indicators = this.$element.find('.carousel-indicators')
