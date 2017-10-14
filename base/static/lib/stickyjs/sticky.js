273f30f4fd9e3a79d2d3ca573288e41ef425ad3d
hriks <hriks@outlook.com>
2017-10-15 01:46:21 +0530

    modified js

--- base/static/lib/stickyjs/sticky.js
+++ base/static/lib/stickyjs/sticky.js
@@ -1,21 +1,7 @@
-// Sticky Plugin v1.0.4 for jQuery
-// =============
-// Author: Anthony Garand
-// Improvements by German M. Bravo (Kronuz) and Ruud Kamphuis (ruudk)
-// Improvements by Leonardo C. Daronco (daronco)
-// Created: 02/14/2011
-// Date: 07/20/2015
-// Website: http://stickyjs.com/
-// Description: Makes an element on the page stick on the screen as you scroll
-//              It will only set the 'top' and 'position' of your element, you
-//              might need to adjust the width in some cases.
-
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
         define(['jquery'], factory);
     } else if (typeof module === 'object' && module.exports) {
-        // Node/CommonJS
         module.exports = factory(require('jquery'));
     } else {
         // Browser globals
@@ -252,8 +238,6 @@
         });
       }
     };
-
-  // should be more efficient than using $window.scroll(scroller) and $window.resize(resizer):
   if (window.addEventListener) {
     window.addEventListener('scroll', scroller, false);
     window.addEventListener('resize', resizer, false);
